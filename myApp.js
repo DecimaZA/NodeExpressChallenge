5c64a0951f663d750230dc95eea4b601b6fcc0cf
Jonathan Wright <devorakwright@gmail.com>
2022-09-12 13:20:02 +0200

    second commit

--- myApp.js
+++ myApp.js
@@ -1,19 +1,13 @@
 let express = require('express');
 let app = express();
+let absolutePath = __dirname + "/views/index.html";
 
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function (req, res) {
+    res.sendFile(absolutePath);
+    }
+);
 
 
 
